ca349382d99fc3ebe3d27c368ff22e2dddeb41f3
mateoarev <mateoarev31@gmail.com>
2024-08-14 16:58:45 -0300

    ejercicio 4 y 5

--- TP1_WebII/Ejercicio4/script.js
+++ TP1_WebII/Ejercicio4/script.js
@@ -1,7 +1,7 @@
-var arregloEstatura = ['1.60', '1.61', '1.62', '1.63', '1.64', '1.65', '1.66', '1.67', '1.68', '1.69'];
+var arregloEstatura = [1.60, 1.61, 1.62, 1.63, 1.64, 1.65, 1.66, 1.67, 1.68, 1.69];
 document.write(arregloEstatura.join(', ') + '<br>');
 
-arregloEstatura.push('1.70');
+arregloEstatura.push(1.70);
 document.write(arregloEstatura.join(', ') + '<br>');
 
 // Elimina los tres últimos elementos del arreglo usando pop() tres veces
--- TP1_WebII/Ejercicio5/script.js
+++ TP1_WebII/Ejercicio5/script.js
@@ -1,4 +1,4 @@
-var arregloEdades = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
+var arregloEdades = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 document.write(arregloEdades.join(', ') + '<br>');
 
 // Elimina los primeros cinco elementos del arreglo usando shift() cinco veces
