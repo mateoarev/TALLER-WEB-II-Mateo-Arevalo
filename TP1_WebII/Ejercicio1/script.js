9529c07c94249ff36c44462e8930f610e825d86d
mateoarev <mateoarev31@gmail.com>
2024-08-14 16:55:06 -0300

    ejercicio 1

--- TP1_WebII/Ejercicio1/script.js
+++ TP1_WebII/Ejercicio1/script.js
@@ -1,4 +1,4 @@
-var arregloEstatura = ['1.60', '1.61', '1.62', '1.63', '1.64', '1.65', '1.66', '1.67', '1.68', '1.69'];
+var arregloEstatura = [1.60, 1.61, 1.62, 1.63, 1.64, 1.65, 1.66, 1.67, 1.68, 1.69];
 document.write(arregloEstatura.join(', ') + '<br>');
 
 arregloEstatura.push('1.70'); // Agrega un elemento al final del arreglo
