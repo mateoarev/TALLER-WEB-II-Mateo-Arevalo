d438c4ed313589d35ba9e8e8bb573935a515b18e
mateoarev <mateoarev31@gmail.com>
2024-09-10 15:42:19 -0300

    proyecto

--- proyecto metodos/script.js
+++ proyecto metodos/script.js
@@ -14,10 +14,10 @@ Utilizaremos el selector individual document.getElementById('')
 //     tercerP.innerHTML = 'Tercer párrafo'
 // }
 
-function escribirTextoCuartoParrafo() {
-    var cuartoP = document.getElementById('cuartoParrafo');
-    cuartoP.innerHTML = 'Cuarto párrafo'
-}
+// function escribirTextoCuartoParrafo() {
+//     var cuartoP = document.getElementById('cuartoParrafo');
+//     cuartoP.innerHTML = 'Cuarto párrafo'
+// }
 
 /*Selector para seleccionar elementos por medio de su id, clase o etiqueta*/
 // var sparrafo = document.querySelector('#segundoParrafo');
@@ -32,8 +32,17 @@ Luego odenar mediante el método sort()
 /*Utilizar un párrafo para mostrar los nombres sin ordenar*/
 var arregloNombres = ['Lourdes', 'Ximena', 'Raúl', 'Ana', 'Eliana', 'Paula'];
 var sinOrdenar = document.querySelector('#segundoParrafo');
-sinOrdenar.innerHTML = arregloNombres.join(' ');
+sinOrdenar.innerHTML = arregloNombres.join(', ');
 
 /*Utilizar un párrafo para imprimir los nombres ordenados en un tercer párrafo*/
 var ordenar = document.querySelector('#tercerParrafo');
-ordenar.innerHTML = arregloNombres.sort().join(' ');
+ordenar.innerHTML = arregloNombres.sort().join(', ');
+
+/*Utilizar un párrafo para mostrar los precios sin aumento*/
+var arregloPrecios = [600, 400, 300, 200, 100];
+var sinAumento = document.querySelector('#quintoParrafo');
+sinAumento.innerHTML = arregloPrecios.join(' / ');
+
+/*Utilizar un párrafo para mostrar los precios con aumento*/
+var aumento = document.querySelector('#sextoParrafo');
+aumento.innerHTML = arregloPrecios.map(precio => (precio * 1.10).toFixed(2)).join(' / ');
